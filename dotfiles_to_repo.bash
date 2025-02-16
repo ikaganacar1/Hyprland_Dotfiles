c0dabea406ad37f9a0476ff845cf40f02336c0de
ikaganacar1 <acarismailkagan@gmail.com>
2025-02-16 21:52:32 +0300

    Update

---
+++ dotfiles_to_repo.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp -r ~/.config/fuckin_firefox_policy/ ~/yzlm/Hyprland_Dotfiles/
+cp -r ~/.config/hypr/ ~/yzlm/Hyprland_Dotfiles/
+cp -r ~/.config/kitty/ ~/yzlm/Hyprland_Dotfiles/
+cp -r ~/.config/nvim/ ~/yzlm/Hyprland_Dotfiles/
+cp -r ~/.config/waybar/ ~/yzlm/Hyprland_Dotfiles/
+cp -r ~/.config/wofi/ ~/yzlm/Hyprland_Dotfiles/
+cp -r ~/.config/fastfetch/ ~/yzlm/Hyprland_Dotfiles/
+
+cp ~/.config/starship.toml ~/yzlm/Hyprland_Dotfiles/starship.toml
+cp ~/.bashrc ~/yzlm/Hyprland_Dotfiles/.bashrc
+cp ~/yzlm/dotfiles_to_repo.bash ~/yzlm/Hyprland_Dotfiles/dotfiles_to_repo.bash
